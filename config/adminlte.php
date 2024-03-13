8bff70d3c8e25942d51085b2ee8a0c33c09991d9
fadlynj <fadlynugrahaj@gmail.com>
2024-03-13 10:59:42 +0700

    Tambah menu halaman kategori di navbar

--- config/adminlte.php
+++ config/adminlte.php
@@ -322,6 +322,11 @@ return [
             'label' => 4,
             'label_color' => 'success',
         ],
+        [
+            'text' => 'Kategori',
+            'url' => 'kategori',
+            'icon' => 'fas fa-fw fa-layer-group',
+        ],
         ['header' => 'account_settings'],
         [
             'text' => 'profile',
